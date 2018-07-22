e3f301081511f14b46fdcc71baff9452d6214ff3
ArielMao <mjjkyy@qq.com>
2018-07-23 00:57:48 +0800

    20180723practice

---
+++ 1_hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/#!/usr/bin/env python3
+
+#print('hello,world');  #first trial
+
+name = input('please enter your name:')
+print 'hello,',name
---
+++ 1_inputTest.py
@@ -0,0 +1,5 @@
+#-*- coding: utf-8 -*-
+
+result = 1024*768
+
+print '1024*768=',result
